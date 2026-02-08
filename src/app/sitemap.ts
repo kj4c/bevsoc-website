1f40385de6f08214cf738039fb5f6c30550d87ae
Ka Ho Wang <kahowang3659@gmail.com>
2026-02-08 12:06:34 +1100

    small change

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,29 +2,30 @@ import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://bevsoc-website.vercel.app';
+  const lastModified = new Date(process.env.VERCEL_GIT_COMMIT_TIMESTAMP ?? Date.now());
 
   return [
     {
       url: `${baseUrl}/`,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
       url: `${baseUrl}/events`,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
       url: `${baseUrl}/about`,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
       url: `${baseUrl}/sponsors`,
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.7,
     },
